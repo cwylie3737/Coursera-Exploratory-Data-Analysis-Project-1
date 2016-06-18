de7cd4bd7b0a2bae764d0eba7ece7879f0347178
cwylie3737 <cwylie3737@gmail.com>
2016-06-18 00:35:44 -0400

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -6,13 +6,13 @@
 setwd("C:/Users/Chuck/Documents/R/coursera/Module 4 - Exploratory Data Analysis/Week 1")
 rm(list=ls())
 
-# Read data file, extract two days data, extract field "Global_active_power"
+# Read data file, extract two days data, extract field "Global_active_power", convert the dates from char
 HPC <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?", stringsAsFactors=FALSE, dec=".")
 HPC <- HPC[HPC$Date %in% c("1/2/2007","2/2/2007") ,]
-DT <- strptime(paste(HPC$Date, HPC$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 GAP <- HPC$Global_active_power
+DT <- strptime(paste(HPC$Date, HPC$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 
-# Open png plot device, plot the histogram, close the plot device
+# Open png plot device, plot the graph, close the plot device
 png("plot2.png", width=480, height=480)
 plot(DT, GAP, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
